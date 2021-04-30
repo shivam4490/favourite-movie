ec88be397fcd95d4e5e08d2a585e2691847a6237
shivam4490 <csg9152@gmail.com>
2021-04-30 19:01:00 +0530

    contols the backdrop and canceladdmovie button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,25 @@
 const addMovieModal = document.getElementById('add-modal');
-
+const backDrop = document.getElementById('backdrop');
 const startAddMovieButton = document.querySelector('header button');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+   backDrop.classList.toggle('visible');
+};
 
 const toggleMovieModal = () => {
    addMovieModal.classList.toggle('visible');
+   toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+   toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+   toggleMovieModal();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backDrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
