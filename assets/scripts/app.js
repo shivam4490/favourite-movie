63e24bbaa622f811de36eee0579172c69461dbca
shivam4490 <csg9152@gmail.com>
2021-04-30 19:26:37 +0530

    fetch and validate user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,8 @@ const addMovieModal = document.getElementById('add-modal');
 const backDrop = document.getElementById('backdrop');
 const startAddMovieButton = document.querySelector('header button');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelector('input');
 
 const toggleBackdrop = () => {
    backDrop.classList.toggle('visible');
@@ -12,8 +14,24 @@ const toggleMovieModal = () => {
    toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
    toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+   const titleValue = userInputs[0].value;
+   const imageUrlValue = userInputs[1].value;
+   const ratingValue = userInputs[2].value;
+
+   if (
+      titleValue.trim() === '' ||
+      imageUrlValue.trim() === '' ||
+      ratingValue === '' ||
+      +ratingValue < 1 ||
+      +rating > 5
+   ) {
+      alert('please enter valid rating values between 1 to 5');
+   }
 };
 
 const backdropClickHandler = () => {
@@ -22,4 +40,5 @@ const backdropClickHandler = () => {
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backDrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener('click');
